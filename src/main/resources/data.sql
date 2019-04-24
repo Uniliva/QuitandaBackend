377e71228ab9a1d806b17fad5ffd0617a2df9495
Uniliva Alves <uniliva@hotmail.com>
2019-04-23 21:58:28 -0300

    Adicionado configurações da base de dados no arquivo properties e removidos arquinos não utilizados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into TIPO_PRODUTO (NOME) values('Frutas'); 
+insert into TIPO_PRODUTO (NOME) values('Legumes'); 
+insert into TIPO_PRODUTO (NOME) values('Verduras'); 
+
+
+insert into PRODUTO (NOME,COD_TIPO_PRODUTO,DESCRICAO,CALORIAS,PESO_MEDIO) values('Maça',1,'Descrição maça',20.2,0.5); 
+insert into PRODUTO (NOME,COD_TIPO_PRODUTO,DESCRICAO,CALORIAS,PESO_MEDIO) values('Beterraba',2,'Descrição Beterraba',30.2,1.5); 
+insert into PRODUTO (NOME,COD_TIPO_PRODUTO,DESCRICAO,CALORIAS,PESO_MEDIO) values('Mandioca',2,'Descrição Mandioca',15.2,3.5); 
+insert into PRODUTO (NOME,COD_TIPO_PRODUTO,DESCRICAO,CALORIAS,PESO_MEDIO) values('Cebola',2,'Descrição Cebola',12.2,3.5); 
+insert into PRODUTO (NOME,COD_TIPO_PRODUTO,DESCRICAO,CALORIAS,PESO_MEDIO) values('Alface',3,'Descrição Alface',5.2,0.7); 
+insert into PRODUTO (NOME,COD_TIPO_PRODUTO,DESCRICAO,CALORIAS,PESO_MEDIO) values('Couve',3,'Descrição Couve',5.2,0.7); 
