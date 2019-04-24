b874300c8074a4f6fccfc04aaaa2f3432bb79bbb
Uniliva Alves <uniliva@hotmail.com>
2019-04-23 23:10:24 -0300

    Adicionando beam para mapper

--- src/main/java/br/com/yaman/quitanda/App.java
+++ src/main/java/br/com/yaman/quitanda/App.java
@@ -1,7 +1,9 @@
 package br.com.yaman.quitanda;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
@@ -9,4 +11,9 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}	
 }
